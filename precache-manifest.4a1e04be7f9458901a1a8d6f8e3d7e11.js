53d75a03b59bd837c27064c43c389ff36a9d689b
Ivan Ivanitskiy <iivanitskiy@mail.ru>
2020-09-25 14:58:11 +0300

    Updates

---
+++ precache-manifest.4a1e04be7f9458901a1a8d6f8e3d7e11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87550b5947a5cb6237e33bfe1c9adbbb",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "24c2e957427bfc4f3611",
+    "url": "/react-test/static/css/main.8a43087d.chunk.css"
+  },
+  {
+    "revision": "630c7e2fc0b62496d380",
+    "url": "/react-test/static/js/2.5e46e06f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-test/static/js/2.5e46e06f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24c2e957427bfc4f3611",
+    "url": "/react-test/static/js/main.b3fa54f8.chunk.js"
+  },
+  {
+    "revision": "8552d2ec2bdfe3d4216d",
+    "url": "/react-test/static/js/runtime-main.16283f8b.js"
+  }
+]);
